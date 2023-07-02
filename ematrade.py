5a3651edc4e6b73cee799d8f52f1e37902f32526
JuginPower <juginpower90@gmail.com>
2023-07-02 09:56:18 +0200

    feature-ematrade: ematrade complete

--- ematrade.py
+++ ematrade.py
@@ -36,29 +36,3 @@ for ax, indi in zip(subplots, indicators):
 
 
 plt.show()
-"""
-my_plotter(ax1, cleaned_dates[-abs(i):-abs(i)+i-1], cleaned_prices[-abs(i):-abs(i)+i-1], {"label": "price", "c": "green"})
-my_plotter(ax2, cleaned_dates[-abs(i):-abs(i)+i-1], cleaned_prices[-abs(i):-abs(i)+i-1], {"label": "price", "c": "green"})
-my_plotter(ax2, cleaned_dates[-abs(i):-abs(i)+i-1], ema8[-abs(i):-abs(i)+i-1], {"label": "ema8", "c": "red"})
-my_plotter(ax2, cleaned_dates[-abs(i):-abs(i)+i-1], ema21[-abs(i):-abs(i)+i-1], {"label": "ema21", "c": "blue"})
-my_plotter(ax2, cleaned_dates[-abs(i):-abs(i)+i-1], ema200[-abs(i):-abs(i)+i-1], {"label": "ema200", "c": "black"})
-
-ax1.legend()
-ax2.legend()
-ax1.tick_params(axis='x', labelrotation=90)
-ax2.tick_params(axis='x', labelrotation=90)
-ax1.grid(True)
-ax2.grid(True)
-
-for index, label in enumerate(ax1.xaxis.get_ticklabels()):
-    if index % n != 0:
-        label.set_visible(False)
-
-for index, label in enumerate(ax2.xaxis.get_ticklabels()):
-    if index % n != 0:
-        label.set_visible(False)
-
-plt.title("DAX - " + timeframe.title())
-plt.show()
-print("Anzahl der Daten:", len(cleaned_dates))
-"""
